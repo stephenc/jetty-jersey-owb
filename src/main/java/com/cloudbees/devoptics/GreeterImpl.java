c8c2d694ed99315399f3866fe34df7adb4431c14
Stephen Connolly <stephen.alan.connolly@gmail.com>
2019-04-24 16:51:50 +0100

    Add in microprofile config implementation

--- src/main/java/com/cloudbees/devoptics/GreeterImpl.java
+++ src/main/java/com/cloudbees/devoptics/GreeterImpl.java
@@ -1,12 +1,18 @@
 package com.cloudbees.devoptics;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class GreeterImpl implements Greeter {
 
+    @Inject
+    @ConfigProperty(name = "greetee.name", defaultValue = "World")
+    String name;
+
     @Override
     public String getMessage() {
-        return "Hello World";
+        return String.format("Hello %s", name);
     }
 }
